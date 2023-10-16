28353620a113fa5287c38ebdee528f924b6dc093
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-16 14:47:47 +0300

    used navigator.clipboard feature + used feature detection and callback code for browser that don't support it

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 const button = document.querySelector('button');
+const textParagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
-  // do something...
+  const text = textParagraph.textContent;
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    alert('Feature not available, please copy manually!');
+  }
 });
