36cda4da3379a814eec8b103d6f7cc67bc84a420
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-17 11:53:10 +0300

    installed corejs + regenerator-runtime

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+
 const button = document.querySelector('button');
 const textParagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
   const text = textParagraph.textContent;
+  const promise = new Promise();
+  console.log(promise);
   if (navigator.clipboard) {
     navigator.clipboard
       .writeText(text)
